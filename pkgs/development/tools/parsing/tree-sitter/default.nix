31354fa10e85fe7142cc956846308ef6ae40cb68
Adrien Faure <adrien.faure@protonmail.com>
2025-01-20 09:31:57 +0100

    tree-sitter-grammar-markdown-inline: flag as broken

--- pkgs/development/tools/parsing/tree-sitter/default.nix
+++ pkgs/development/tools/parsing/tree-sitter/default.nix
@@ -119,6 +119,7 @@ let
           tree-sitter-markdown-inline = grammars'.tree-sitter-markdown // {
             language = "tree-sitter-markdown_inline";
             location = "tree-sitter-markdown-inline";
+            isBroken = true;
           };
         }
         // {
